8e588f771552505adb26dbed69f204fb32672149
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-06-05 19:00:18 +0000

    feat: changement du fonctionnement du fichier config

--- proxy.js
+++ proxy.js
@@ -4,12 +4,12 @@ const fs = require("fs");
 
 const config = JSON.parse(fs.readFileSync("config.json"));
 
-const wss = new WebSocket.Server({ port: config.wsPort });
+const wss = new WebSocket.Server({ port: 80 });
 
 wss.on("connection", (ws) => {
     const tcp = new net.Socket();
 
-    tcp.connect(config.tcpPort, config.tcpHost, () => {
+    tcp.connect(config.tcp.port, config.tcp.host, () => {
         ws.on("message", (msg) => tcp.write(msg));
         tcp.on("data", (data) => ws.send(data));
         ws.on("close", () => tcp.end());
